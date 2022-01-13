35eaa824a74909be8f0f36528b0b49e2f754d0e0
Jens Ljungblad <jens.ljungblad@gmail.com>
2022-01-13 10:14:26 -0500

    Make use of the timeout constant

--- lib/fcm.rb
+++ lib/fcm.rb
@@ -224,7 +224,7 @@ class FCM
   private
 
   def for_uri(uri, extra_headers = {})
-    connection = ::Faraday.new(:url => uri) do |faraday|
+    connection = ::Faraday.new(:url => uri, :request => { timeout: DEFAULT_TIMEOUT }) do |faraday|
       faraday.adapter Faraday.default_adapter
       faraday.headers["Content-Type"] = "application/json"
       faraday.headers["Authorization"] = "key=#{api_key}"
